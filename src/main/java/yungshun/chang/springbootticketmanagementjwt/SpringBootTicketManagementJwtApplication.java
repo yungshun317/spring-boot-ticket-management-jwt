1c2fcfcc1bf590825691428b61723ee4b5336f7b
yungshun317 <seraph317@gmail.com>
2018-12-06 11:05:16 +0800

    feat: create a Spring Boot project using Spring Initializr via IntelliJ

---
+++ src/main/java/yungshun/chang/springbootticketmanagementjwt/SpringBootTicketManagementJwtApplication.java
@@ -0,0 +1,14 @@
+package yungshun.chang.springbootticketmanagementjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTicketManagementJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTicketManagementJwtApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/yungshun/chang/springbootticketmanagementjwt/SpringBootTicketManagementJwtApplicationTests.java
@@ -0,0 +1,17 @@
+package yungshun.chang.springbootticketmanagementjwt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootTicketManagementJwtApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
