c6c7c67d5fe84e1aa756cd19597fe5cb05b45433
yungshun317 <seraph317@gmail.com>
2018-12-08 11:16:50 +0800

    feat: create UserController and fill in all methods for CRUD user operations

---
+++ src/main/java/yungshun/chang/springbootticketmanagementjwt/Controller/HomeController.java
@@ -0,0 +1,21 @@
+package yungshun.chang.springbootticketmanagementjwt.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @ResponseBody
+    @RequestMapping("")
+    public Map<String, Object> test() {
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("result", "Aloha");
+        return map;
+    }
+}
---
+++ src/main/java/yungshun/chang/springbootticketmanagementjwt/Controller/UserController.java
@@ -0,0 +1,60 @@
+package yungshun.chang.springbootticketmanagementjwt.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import yungshun.chang.springbootticketmanagementjwt.model.User;
+import yungshun.chang.springbootticketmanagementjwt.service.UserService;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserService userSevice;
+
+    @ResponseBody
+    @RequestMapping("")
+    public List<User> getAllUsers() {
+        return userSevice.getAllUsers();
+    }
+
+    @ResponseBody
+    @RequestMapping("/{id}")
+    public User getUser(@PathVariable("id") Integer id) {
+        return userSevice.getUser(id);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public Map<String, Object> createUser(@RequestParam(value = "userid") Integer userid,
+                                          @RequestParam(value = "username") String username) {
+        Map<String, Object> map = new LinkedHashMap<>();
+        userSevice.createUser(userid, username);
+        map.put("result", "added");
+        return map;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "", method = RequestMethod.PUT)
+    public Map<String, Object> updateUser(@RequestParam(value = "userid") Integer userid,
+                                          @RequestParam(value = "username") String username) {
+        Map<String, Object> map = new LinkedHashMap<>();
+        userSevice.updateUser(userid, username);
+        map.put("result", "updated");
+        return map;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public Map<String, Object> deleteUser(
+            @PathVariable("id") Integer userid) {
+        Map<String, Object> map = new LinkedHashMap<>();
+        userSevice.deleteUser(userid);
+        map.put("result", "deleted");
+        return map;
+    }
+}
