2300a18652101ad93619a436a9a725a85323af72
yungshun317 <seraph317@gmail.com>
2018-12-09 14:14:55 +0800

    feat: generate a token and get a subject from a JWT token

--- src/main/java/yungshun/chang/springbootticketmanagementjwt/Controller/HomeController.java
+++ src/main/java/yungshun/chang/springbootticketmanagementjwt/Controller/HomeController.java
@@ -1,8 +1,11 @@
 package yungshun.chang.springbootticketmanagementjwt.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import yungshun.chang.springbootticketmanagementjwt.service.SecurityService;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -11,11 +14,38 @@ import java.util.Map;
 @RequestMapping("/")
 public class HomeController {
 
+    @Autowired
+    SecurityService securityService;
+
     @ResponseBody
     @RequestMapping("")
     public Map<String, Object> test() {
         Map<String, Object> map = new LinkedHashMap<>();
         map.put("result", "Aloha");
+        return map;
+    }
+
+    @ResponseBody
+    @RequestMapping("/security/generate/token")
+    public Map<String, Object> generateToken(@RequestParam(value="subject") String subject) {
+
+        String token = securityService.createToken(subject, (15 * 1000 * 60));
+
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("result", token);
+
+        return map;
+    }
+
+    @ResponseBody
+    @RequestMapping("/security/get/subject")
+    public Map<String, Object> getSubject(@RequestParam(value="token") String token) {
+
+        String subject = securityService.getSubject(token);
+
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("result", subject);
+
         return map;
     }
 }
